12a4926898770e560c6fa48da01c9209f87b6de1
UndetectedCheats <61540892+kasivit8@users.noreply.github.com>
2021-04-11 16:29:26 +0700

    Set usable options for editors, add select theme (dummy)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import ReactMarkdown from "react-markdown";
 import "antd/dist/antd.css";
-import { Collapse, Menu, Layout, Upload, message } from 'antd';
+import { Collapse, Menu, Select, Layout, Upload, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 
 import { Editors } from './Editor';
@@ -11,8 +11,10 @@ import { Editors } from './Editor';
 const { Panel } = Collapse;
 const { Header, Content} = Layout;
 const { Dragger } = Upload;
+const { Option } = Select;
 
-const props = {
+//for uploading files
+const Dragger_props = {
   name: 'file',
   multiple: true,
   action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
@@ -29,9 +31,31 @@ const props = {
   },
 };
 
+function handleClick() {
+  console.log('Click happened');
+}
+
+//for select option
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+function onSearch(val) {
+  console.log('search:', val);
+}
+function selectTheme(value) {
+  console.log(`selected ${value}`);
+  try {
+    //set theme option
+  } catch(e) {
+    console.log('error',e);
+  }
+}
+
 function App() {
+
   const markdown = `
-  ## Python lab 1
+  # PythonLab 1
+  ## Question 1
   Make a **Palindrome** with _O(N)_ time complexity.
   ***
   `;
@@ -50,17 +74,119 @@ function App() {
               <div className="logo" />
               <Menu 
               theme="dark" 
-              mode="horizontal" 
-              defaultSelectedKeys={["1"]}>
-                <Menu.Item key="1">C++</Menu.Item>
-                <Menu.Item key="2">Python</Menu.Item>
-                <Menu.Item key="3">Java</Menu.Item>
+              mode="horizontal"
+              selectable="false"
+              //defaultSelectedKeys={["1"]}
+              >
+                <Menu.Item key="1">
+                  <Select 
+                  //defaultValue="editor" 
+                  placeholder="Select upload type"
+                  style={{ width: 180, padding: 10 }} 
+                  onChange={handleChange}
+                  >
+                    <Option value="editor">Editor</Option>
+                    <Option value="upload_files">Upload_files</Option>
+                  </Select>
+                  <Select 
+                  showSearch
+                  //defaultValue="C++" 
+                  placeholder="Select a language"
+                  style={{ width: 180, padding: 10 }} 
+                  onChange={handleChange}
+                  onSearch={onSearch}
+                  >
+                    <Option value="c++">C++</Option>
+                    <Option value="java">Java</Option>
+                    <Option value="python" disabled>
+                      Python
+                    </Option>
+                    <Option value="disabled" disabled>
+                      Disabled
+                    </Option>
+                  </Select>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Select 
+                  showSearch
+                  defaultValue="default" 
+                  placeholder="Select Editor Theme"
+                  style={{ width: 180, padding: 10 }} 
+                  onChange={selectTheme}
+                  onSearch={onSearch}
+                  >
+                    <Option disabled>Select Editor Theme</Option>
+                    <Option value="default">default</Option>
+                    <Option value="3024-day">3024-day</Option>
+                    <Option value="3024-night">3024-night</Option>
+                    <Option value="abcdef">abcdef</Option>
+                    <Option value="ambiance">ambiance</Option>
+                    <Option value="ayu-dark">ayu-dark</Option>
+                    <Option value="ayu-mirage">ayu-mirage</Option>
+                    <Option value="base16-dark">base16-dark</Option>
+                    <Option value="base16-light">base16-light</Option>
+                    <Option value="bespin">bespin</Option>
+                    <Option value="blackboard">blackboard</Option>
+                    <Option value="cobalt">cobalt</Option>
+                    <Option value="colorforth">colorforth</Option>
+                    <Option value="darcula">darcula</Option>
+                    <Option value="dracula">dracula</Option>
+                    <Option value="duotone-dark">duotone-dark</Option>
+                    <Option value="duotone-light">duotone-light</Option>
+                    <Option value="eclipse">eclipse</Option>
+                    <Option value="elegant">elegant</Option>
+                    <Option value="erlang-dark">erlang-dark</Option>
+                    <Option value="gruvbox-dark">gruvbox-dark</Option>
+                    <Option value="hopscotch">hopscotch</Option>
+                    <Option value="icecoder">icecoder</Option>
+                    <Option value="idea">idea</Option>
+                    <Option value="isotope">isotope</Option>
+                    <Option value="lesser-dark">lesser-dark</Option>
+                    <Option value="liquibyte">liquibyte</Option>
+                    <Option value="lucario">lucario</Option>
+                    <Option value="material">material</Option>
+                    <Option value="material-darker">material-darker</Option>
+                    <Option value="material-palenight">material-palenight</Option>
+                    <Option value="material-ocean">material-ocean</Option>
+                    <Option value="mbo">mbo</Option>
+                    <Option value="mdn-like">mdn-like</Option>
+                    <Option value="midnight">midnight</Option>
+                    <Option value="monokai">monokai</Option>
+                    <Option value="moxer">moxer</Option>
+                    <Option value="neat">neat</Option>
+                    <Option value="neo">neo</Option>
+                    <Option value="night">night</Option>
+                    <Option value="nord">nord</Option>
+                    <Option value="oceanic-next">oceanic-next</Option>
+                    <Option value="panda-syntax">panda-syntax</Option>
+                    <Option value="paraiso-dark">paraiso-dark</Option>
+                    <Option value="paraiso-light">paraiso-light</Option>
+                    <Option value="pastel-on-dark">pastel-on-dark</Option>
+                    <Option value="railscasts">railscasts</Option>
+                    <Option value="rubyblue">rubyblue</Option>
+                    <Option value="seti">seti</Option>
+                    <Option value="shadowfox">shadowfox</Option>
+                    <Option value="solarized dark">solarized dark</Option>
+                    <Option value="solarized light">solarized light</Option>
+                    <Option value="the-matrix">the-matrix</Option>
+                    <Option value="tomorrow-night-bright">tomorrow-night-bright</Option>
+                    <Option value="tomorrow-night-eighties">tomorrow-night-eighties</Option>
+                    <Option value="ttcn">ttcn</Option>
+                    <Option value="twilight">twilight</Option>
+                    <Option value="vibrant-ink">vibrant-ink</Option>
+                    <Option value="xq-dark">xq-dark</Option>
+                    <Option value="xq-light">xq-light</Option>
+                    <Option value="yeti">yeti</Option>
+                    <Option value="yonce">yonce</Option>
+                    <Option value="zenburn">zenburn</Option>
+                  </Select>
+                </Menu.Item>
               </Menu>
             </Header>
             
             <Content style={{ textAlign: 'left' }}>
               <Editors />
-              <Dragger {...props}>
+              <Dragger {...Dragger_props}>
                 <p className="ant-upload-drag-icon">
                   <InboxOutlined />
                 </p>
@@ -71,7 +197,12 @@ function App() {
                 </p>
               </Dragger>
             </Content>
+            <button onClick={handleClick}>Upload File</button>
           </Layout>
+        </Panel>
+
+        <Panel header="Result" key="Result">
+          <p>lorem</p>
         </Panel>
       </Collapse>
     </div>
@@ -91,4 +222,6 @@ https://www.youtube.com/watch?v=fF9vrZNSQC4&ab_channel=CoderOne
 https://www.newline.co/@dmitryrogozhny/how-to-render-markdown-in-react-with-react-markdown--5d1c3849
 https://reactjs.org/tutorial/tutorial.html#setup-for-the-tutorial
 https://github.com/scniro/react-codemirror2/issues/83
+https://github.com/JedWatson/react-codemirror
+https://codemirror.net/doc/manual.html#config
 */
--- src/Editor.js
+++ src/Editor.js
@@ -5,6 +5,21 @@ require("codemirror/lib/codemirror.css");
 require("codemirror/mode/python/python");
 require("codemirror/mode/clike/clike");
 
+//require all css files in "codemirror/theme"
+var themes = require.context('../node_modules/codemirror/theme', false, /\.css$/);
+themes.keys().forEach(themes);
+
+//reqire codemirror addons
+require("codemirror/addon/selection/active-line");
+require("codemirror/addon/edit/closebrackets");
+require("codemirror/addon/edit/matchbrackets");
+require("codemirror/addon/fold/foldcode");
+require("codemirror/addon/fold/foldgutter");
+require("codemirror/addon/fold/foldgutter.css");
+require("codemirror/addon/fold/brace-fold");
+require("codemirror/addon/fold/comment-fold");
+require("codemirror/addon/fold/indent-fold");
+
 const DEFAULT_CPP_VALUE = `#include "RandomIntGenerator.hpp"
 RandomIntGenerator::RandomIntGenerator() {
 	*this = RandomIntGenerator(0, INT_MAX);
@@ -13,28 +28,28 @@ RandomIntGenerator::RandomIntGenerator(int min, int max) {
 	setMaxMin(min, max); srand(time(NULL));
 }`
 
-const DEFAULT_PY_VALUE = `class Building:
-def __init__(self,address="1234 Main St.",sq_ft="925 sq ft"):
+const DEFAULT_PYTHON_VALUE = `class Building:
+  def __init__(self,address="1234 Main St.",sq_ft="925 sq ft"):
     self.address=address
     self.sq_ft=sq_ft
-def __repr__(self):
+  def __repr__(self):
     return "Building:" + self.address + self.sq_ft
 
 class Residential(Building):
-def __init__(self,address="1234 Main St.",sqft="925 sq ft",num_bedrooms=1):
+  def __init__(self,address="1234 Main St.",sqft="925 sq ft",num_bedrooms=1):
     super().__init__()
     self.num_bedrooms= num_bedrooms
-def __repr__(self):
-    return "Building:" + self.address + self.sq_ft + "\nResidential:" + str(self.num_bedrooms)
+  def __repr__(self):
+    return "Building:" + self.address + self.sq_ft + "\\nResidential:" + str(self.num_bedrooms)
 
 if __name__ == "__main__":
-B1=Building()
-R1=Residential()
-print("Building Class:")
-print(B1)
-print()
-print("Residential Class:")
-print(R1)
+  B1=Building()
+  R1=Residential()
+  print("Building Class:")
+  print(B1)
+  print()
+  print("Residential Class:")
+  print(R1)
 `
 
 const DEFAULT_JAVA_VALUE = `public class Mediator {
@@ -66,44 +81,56 @@ const DEFAULT_JAVA_VALUE = `public class Mediator {
 `
 
 const DEFAULT_CPP_OPTIONS = {
+  mode: "text/x-c++src",
+  theme: "darcula",
   autoCloseBrackets: true,
   matchBrackets: true,
+  highlightSelectionMatches: true,
+  styleActiveLine: true,
+  foldGutter: true,
+  gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
   cursorScrollMargin: 48,
-  mode: "text/x-c++src",
   lineNumbers: true,
   indentUnit: 2,
   tabSize: 2,
-  styleActiveLine: true,
-  viewportMargin: 99
 };
 
-const DEFAULT_PY_OPTIONS = {
-  mode: {name: "python",version: 3,singleLineStringErrors: false},
-  lineNumbers: true,
-  highlightSelectionMatches: true,
-  indentUnit: 4,
-  tabSize: 4,
-  lineWrapping: true,
-  matchBrackets: true,
+const DEFAULT_PYTHON_OPTIONS = {
+  mode: "python",
+  theme: "twilight",
   autoCloseBrackets: true,
+  matchBrackets: true,
+  highlightSelectionMatches: true,
+  styleActiveLine: true,
+  foldGutter: true,
+  gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+  cursorScrollMargin: 48,
+  lineNumbers: true,
+  indentUnit: 2,
+  tabSize: 2,
+  spellcheck: true,
 };
 
 const DEFAULT_JAVA_OPTIONS = {
+  mode: "text/x-java",
+  theme: "panda-syntax",
   autoCloseBrackets: true,
   matchBrackets: true,
+  highlightSelectionMatches: true,
+  styleActiveLine: true,
+  foldGutter: true,
+  gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
   cursorScrollMargin: 48,
-  mode: "text/x-java",
   lineNumbers: true,
   indentUnit: 2,
   tabSize: 2,
-  styleActiveLine: true,
-  viewportMargin: 99
 };
 
 export class Editors extends React.Component {
+
   state = {
     cppValue: DEFAULT_CPP_VALUE || this.props.cppValue,
-    pyValue: DEFAULT_PY_VALUE || this.props.pyValue,
+    pythonValue: DEFAULT_PYTHON_VALUE || this.props.pythonValue,
     javaValue: DEFAULT_JAVA_VALUE || this.props.javaValue,
   };
   
@@ -112,9 +139,9 @@ export class Editors extends React.Component {
     ...this.props.cppOptions
   };
 
-  pyOptions = {
-    ...DEFAULT_PY_OPTIONS,
-    ...this.props.pyOptions
+  pythonOptions = {
+    ...DEFAULT_PYTHON_OPTIONS,
+    ...this.props.pythonOptions
   };
 
   javaOptions = {
@@ -125,6 +152,11 @@ export class Editors extends React.Component {
   onChange = which => (editor, data, value) => {
     this.setState({ [`${which}Value`]: value });
   };
+
+  setTheme(value) {
+    this.editor.setOption('theme', value);
+  }
+
   
   render() {
     return (
@@ -135,6 +167,12 @@ export class Editors extends React.Component {
           options={this.cppOptions}
           onChange={this.onChange("cpp")}
         />
+        
+      </React.Fragment>
+    );
+  }
+}
+/*
         <PureEditor
           name="java"
           value={this.state.javaValue}
@@ -143,14 +181,11 @@ export class Editors extends React.Component {
         />
         <PureEditor
           name="python"
-          value={this.state.pyValue}
-          options={this.pyOptions}
+          value={this.state.pythonValue}
+          options={this.pythonOptions}
           onChange={this.onChange("python")}
         />
-      </React.Fragment>
-    );
-  }
-}
+*/
 class PureEditor extends React.PureComponent {
   render() {
     console.log(`rendering -> ${this.props.name}`);
